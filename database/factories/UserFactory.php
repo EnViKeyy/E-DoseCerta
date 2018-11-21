172bba59a16d82b21bedcd6c11ca3c7351cbfb49
EnViKeyy <pierri.nvk@outlook.com>
2018-11-21 00:38:11 -0200

    Adicionado campo crmv

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\User;
+
 use Faker\Generator as Faker;
 
 /*
@@ -13,11 +15,12 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\User::class, function (Faker $faker) {
+$factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
+        'crmv' => $faker->numberBetween(9999),
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
     ];
