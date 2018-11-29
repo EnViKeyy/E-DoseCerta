25a699d2430ec0327253181f0d58e34be6c31412
EnViKeyy <pierri.nvk@outlook.com>
2018-11-29 11:57:21 -0200

    Implementado rotas ara utilização do AdminLTE

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,30 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+// User routes
+//$this->group(['middleware' => ['auth'], 'namespace' => 'User', 'prefix' => 'users'], function() {
+//    $this->get('/me', 'UserInfo')->name('user.profile');
+//    $this->put('/me', 'UserInfoUpdate')->name('user.profile.update');
+//
+//    $this->group(['middleware' => ['permission:register-employee']], function () {
+//        $this->get('/create', 'UserCreate')->name('users.create');
+//        $this->post('/', 'UserStore')->name('users.store');
+//    });
+//    $this->group(['middleware' => ['permission:update-employee']], function () {
+//        $this->get('/{user}/edit', 'UserEdit')->name('users.edit');
+//        $this->put('/{user}', 'UserUpdate')->name('users.update');
+//    });
+//    $this->group(['middleware' => ['permission:read-employee']], function () {
+//        $this->get('/list', 'UserList')->name('users.list');
+//        $this->get('/', 'UserIndex')->name('users.index');
+//        $this->get('/{user}', 'UserShow')->name('users.show');
+//    });
+//    $this->group(['middleware' => ['permission:delete-employee']], function () {
+//        $this->delete('/{user}', 'UserDestroy')->name('users.destroy');
+//    });
+//});
