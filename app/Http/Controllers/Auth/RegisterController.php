52252471169ef5113ee8d84c43cc69c155e14066
EnViKeyy <pierri.nvk@outlook.com>
2019-01-21 21:07:23 -0200

    Adicionado atributo CRMV para registro de usuários

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\User;
+use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -50,6 +50,7 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
+            'crmv' => ['required', 'string', 'max:4', 'unique:users'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:6', 'confirmed'],
         ]);
@@ -58,15 +59,16 @@ class RegisterController extends Controller
     /**
      * Create a new user instance after a valid registration.
      *
-     * @param  array  $data
-     * @return \App\User
+     * @param array $data
+     * @return mixed
      */
     protected function create(array $data)
     {
         return User::create([
             'name' => $data['name'],
+            'crmv' => $data['crmv'],
             'email' => $data['email'],
-            'password' => Hash::make($data['password']),
+            'password' => $data['password']
         ]);
     }
 }
