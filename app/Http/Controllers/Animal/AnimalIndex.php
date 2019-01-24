bc84545f3ca8962e10e1b31f66d6a65e7d587f8c
EnViKeyy <pierri.nvk@outlook.com>
2019-01-24 12:32:05 -0200

    Adicionado loop para alterar specie de integer para object

--- app/Http/Controllers/Animal/AnimalIndex.php
+++ app/Http/Controllers/Animal/AnimalIndex.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Animal;
 
 use App\Http\Controllers\Controller;
 use App\Models\Animal;
+use App\Models\Specie;
 use Illuminate\Http\Request;
 
 class AnimalIndex extends Controller
@@ -11,16 +12,21 @@ class AnimalIndex extends Controller
     private $itemsPerPage = 10;
 
     /**
+     * @param Request $request
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function __invoke(Request $request)
     {
         $perPage = $request->query->get('perPage', $this->itemsPerPage);
 
-        $animais = Animal::paginate($perPage);
+        $animals = Animal::paginate($perPage);
+
+        foreach ($animals as $animal) {
+            $animal->specie = Specie::findOrFail($animal->specie);
+        }
 
         return view('animals.index', [
-            'animals' => $animais
+            'animals' => $animals
         ]);
     }
 }
