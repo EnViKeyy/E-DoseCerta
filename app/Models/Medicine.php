0b29f0831c9871f95a1a5c3c2efca62d3b1981c3
EnViKeyy <pierri.nvk@outlook.com>
2019-01-28 01:11:04 -0200

    Implementado modelo para medicine

---
+++ app/Models/Medicine.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+
+class Medicine extends Model
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'concentration'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at'
+    ];
+}
